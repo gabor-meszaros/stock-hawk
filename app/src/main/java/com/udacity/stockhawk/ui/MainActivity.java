4e763ffa75399be707a1f325e83d043333fa21d6
Gabor Meszaros <gabor-meszaros@users.noreply.github.com>
2017-04-20 06:18:34 +0200

    Prepare for providing full info for stock details activity

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         Timber.d("Symbol clicked: %s", symbol);
 
         final Intent showStockDetailsIntent = new Intent(this, StockDetailsActivity.class);
+        showStockDetailsIntent.putExtra(StockDetailsActivity.EXTRA_STOCK_SYMBOL, symbol);
         startActivity(showStockDetailsIntent);
     }
 
--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailsActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailsActivity.java
@@ -1,15 +1,32 @@
 package com.udacity.stockhawk.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.udacity.stockhawk.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class StockDetailsActivity extends AppCompatActivity {
+
+    public static final String EXTRA_STOCK_SYMBOL = "extra-stock-name";
+
+    @BindView(R.id.stock_details_symbol)
+    TextView mSymbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_details);
+        ButterKnife.bind(this);
+
+        final Intent intent = getIntent();
+        if (null != intent && intent.hasExtra(EXTRA_STOCK_SYMBOL)) {
+            final String symbol= intent.getStringExtra(EXTRA_STOCK_SYMBOL);
+            mSymbol.setText(symbol);
+        }
     }
 }
